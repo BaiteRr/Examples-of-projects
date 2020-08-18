1e436032ff13859468379c14d4378e6960c27d24
lysak.ilya <ilsy@brighta.biz>
2020-08-18 14:00:28 +0300

    hz

--- src/js/animation.js
+++ src/js/animation.js
@@ -15,9 +15,9 @@ const markImg = document.querySelector('.firstLine__markImg')
 window.onscroll = function () {
     if (window.pageYOffset > 750) {
         header.style.cssText = 'position: absolute; margin-top: 800px'
-        slider.style.cssText = 'padding-top: 156px; margin-top: 0'
-        geo.style.cssText = 'background-image: url("../img/bearth.png")'
+        slider.style.cssText = 'padding-top: 191px; margin-top: 0'
         subGeo.style.color = '#005FA3'
+        geo.style.cssText = 'background-image: url("../img/bearth.png")'
         firstLine.style.color = 'black'
         subnav1.style.color = 'black'
         subnav2.style.color = 'black'
@@ -29,7 +29,6 @@ window.onscroll = function () {
         markImg.style.cssText = 'background-image: url("../img/bmark.png")'
     } else if (window.pageYOffset > 10) {
         header.style.cssText = 'position: absolute; margin-top: 10px'
-        slider.style.cssText = 'padding-top: 156px; margin-top: 0'
         subGeo.style.color = '#FFFFFF'
         geo.style.cssText = 'background-image: url("../img/earth.png")'
         firstLine.style.color = '#FFFFFF'
